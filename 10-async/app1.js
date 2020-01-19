eacd38a4c07def1f1f1a3976c17fcb4b4d90d5e7
Traian <niculescu.traian.ct@gmail.com>
2020-01-19 15:14:46 +0200

    beginning promises

--- 10-async/app1.js
+++ 10-async/app1.js
@@ -1,13 +1,32 @@
-const request = new XMLHttpRequest();
+const getTodos = (resource, callback) => {
+  const request = new XMLHttpRequest();
 
-request.addEventListener('readystatechange', () => {
-  // the state that the current request is in -> readyState
-  console.log(request, request.readyState);
+  // we can track the progress of the request using an event listener -> readystatechange
+  request.addEventListener('readystatechange', () => {
+    // the state that the current request is in -> readyState
+    // console.log(request, request.readyState);
+    if (request.readyState === 4 && request.status === 200) {
+      // console.log(request.responseText);
+      const data = JSON.parse(request.responseText);
+      callback(undefined, data);
+    } else if (request.readyState === 4) {
+      // console.log('some error occured');
+      callback('could not fetch data', undefined);
+    }
+  });
+
+  // setting up the request
+  request.open('GET', resource);
+  // sending the request
+  request.send();
+};
+
+getTodos('todos/luigi.json', (err, data) => {
+  console.log(data);
+  getTodos('todos/mario.json', (err, data) => {
+    console.log(data);
+    getTodos('todos/shaun.json', (err, data) => {
+      console.log(data);
+    });
+  });
 });
-
-// setting up the request
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
-// sending the request
-request.send();
-
-// we can track the progress of the request using an event listener -> readystatechange
