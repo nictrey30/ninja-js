15b8da1ca1eea27f689a1b3b26a769ee8bf89603
Traian <niculescu.traian.ct@gmail.com>
2020-05-07 07:49:27 +0300

    test ssh with l31 file - take 3

--- 1/L31-arg-params.js
+++ 1/L31-arg-params.js
@@ -1,6 +1,8 @@
 // arguments and parameters
-const speak = (name) => {
-  console.log(`good day ${name}`);
+const speak = (name = 'John', time = 'night') => {
+  name = name[0].toUpperCase() + name.substr(1, name.length);
+  console.log(`good ${time.toLowerCase()} ${name}`);
 };
 
-speak('trey');
+speak('trey', 'Morning'); // -> argument
+speak();
---
+++ 1/L32-returning-values.js
@@ -0,0 +1,5 @@
+// returning values
+const calcArea = (radius) => Math.PI * radius ** 2;
+
+const area = calcArea(5);
+console.log(area.toFixed(2));
