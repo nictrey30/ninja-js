c5c2307447a72bfcbbb73fa6426d02cb1034800e
Traian <niculescu.traian.ct@gmail.com>
2020-05-20 08:41:41 +0300

    update

--- 9-dates-2/digital-clock.js
+++ 9-dates-2/digital-clock.js
@@ -1,2 +1,14 @@
-
-
+const clock = document.querySelector('.clock');
+const tick = () => {
+  const now = new Date();
+  const h = now.getHours();
+  const m = now.getMinutes();
+  const s = now.getSeconds();
+  const html = `
+    <span>${('0' + h).slice(-2)}</span> :
+    <span>${('0' + m).slice(-2)}</span> :
+    <span>${('0' + s).slice(-2)}</span>
+  `;
+  clock.innerHTML = html;
+};
+setInterval(tick, 1000);
