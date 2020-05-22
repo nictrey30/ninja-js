ede5df321889c9762af04f58c93efe21366f58bd
Traian <niculescu.traian.ct@gmail.com>
2020-05-22 10:25:25 +0300

    update

--- 10-async-2/L96-async-await.js
+++ 10-async-2/L96-async-await.js
@@ -2,4 +2,9 @@
 const getTodos = async () => {
   // fetch('todos/luigi.json').then(() => {})
   const response = await fetch('todos/luigi.json');
+  const data = await response.json();
+  return data;
 };
+getTodos()
+  .then((data) => console.log('resolved: ', data))
+  .catch((err) => console.log('rejected: ', err.message));
