7f0754cba8d3fbea213c90e34e499cdc952adba6
Traian <niculescu.traian.ct@gmail.com>
2020-05-26 00:00:45 +0300

    update weather app

---
+++ weather-app/scripts/app.js
@@ -0,0 +1,43 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+  const cityDetails = data.cityDetails;
+  const weather = data.weather;
+
+  // update details template
+  details.innerHTML = `
+  <h5 class="my-3">${cityDetails.EnglishName}</h5>
+  <div class="my-3">${weather.WeatherText}</div>
+  <div class="display-4 my-4">
+  <span>${weather.Temperature.Metric.Value}</span>
+  <span>&deg;C</span>
+  </div>
+  `;
+
+  // remove the d-none class if present
+  if (card.classList.contains('d-none')) {
+    card.classList.remove('d-none');
+  }
+};
+
+const updateCity = async (city) => {
+  const cityDetails = await getCity(city);
+  const weather = await getWeather(cityDetails.Key);
+  return { cityDetails, weather };
+};
+
+cityForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  // get the city value
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
+
+  // update the ui with the new citys
+  updateCity(city)
+    .then((data) => {
+      updateUI(data);
+    })
+    .catch((err) => console.log(err));
+});
--- weather-app/scripts/forecast.js
+++ weather-app/scripts/forecast.js
@@ -1,5 +1,16 @@
 const key = 'CXmb8Oz3Gi2nutFcG3OI3kEdN8K4BOPI';
 
+// get weather info
+const getWeather = async (id) => {
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+// get city info
 const getCity = async (city) => {
   const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
   const query = `?apikey=${key}&q=${city}`;
@@ -10,6 +21,11 @@ const getCity = async (city) => {
   return data[0];
 };
 
-getCity('manchester')
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+// getCity('manchester')
+//   .then((data) => {
+//     return getWeather(data.Key);
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => console.log(err));
