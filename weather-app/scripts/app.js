f22d5acd2b54c2426b4c3337b19e278ae680463b
Traian <niculescu.traian.ct@gmail.com>
2020-05-26 17:23:10 +0300

    update

--- weather-app/scripts/app.js
+++ weather-app/scripts/app.js
@@ -1,10 +1,15 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
 
 const updateUI = (data) => {
-  const cityDetails = data.cityDetails;
-  const weather = data.weather;
+  // const cityDetails = data.cityDetails;
+  // const weather = data.weather;
+
+  // destructure properties
+  const { cityDetails, weather } = data;
 
   // update details template
   details.innerHTML = `
@@ -16,10 +21,17 @@ const updateUI = (data) => {
   </div>
   `;
 
+  // update the night/day & icon images
+  let timeSrc = null;
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute('src', iconSrc);
+  timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+
   // remove the d-none class if present
   if (card.classList.contains('d-none')) {
     card.classList.remove('d-none');
   }
+  time.setAttribute('src', timeSrc);
 };
 
 const updateCity = async (city) => {
@@ -40,4 +52,13 @@ cityForm.addEventListener('submit', (e) => {
       updateUI(data);
     })
     .catch((err) => console.log(err));
+
+  // set local storage
+  localStorage.setItem('city', city);
 });
+
+if (localStorage.getItem('city')) {
+  updateCity(localStorage.getItem('city'))
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err));
+}
